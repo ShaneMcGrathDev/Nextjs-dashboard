b8b48831c095cf5465e2d6ebbade94e064628230
Shane <shane.mcgrath.28@gmail.com>
2024-07-14 14:41:43 -0400

    troubleshoot build task at deployment

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+// export const roboto_mono = Roboto_Mono({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+});
